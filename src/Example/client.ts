dbfaf812f8c7ecedf477600fdfb0450a7b1295c6
Klarulor <klarulor@gmail.com>
2021-12-05 22:09:27 +0200

    exp update

--- src/Example/client.ts
+++ src/Example/client.ts
@@ -2,8 +2,8 @@ import { MyWebSocketClient } from "../MyWebSocketClient";
 
 let client: MyWebSocketClient = new MyWebSocketClient("test", "localhost:8002");
 client.connect(() => console.log("success"));
-q()
-async function q(): Promise<void>{
-    let data = await client.get("test", "8002");
+test()
+async function test(): Promise<void>{
+    let data = await client.get("key", "some content");
     console.log(data);
 }
